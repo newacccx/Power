4cb4a1a0c2b45a8a0cbfe32afff3a2a83bcd5b88
CatFishKiller <138358454+CatFishKiller@users.noreply.github.com>
2023-08-13 07:41:12 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+
+APP_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+DEV = 1664850827
+OWNER = ""
+ffmpegcode = ["-preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2  -ab 32k  -vbr 2 -level 3.1 -threads 1"]
+THUMB = "https://telegra.ph/file/3ef8ee967488070b886f2.jpg"

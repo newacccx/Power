1fdd631addf6cded08b780a0eb7145eaf5037b3e
CatFishKiller <138358454+CatFishKiller@users.noreply.github.com>
2023-08-13 08:01:43 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "5704942988:AAFuv7ZkxzGJVyxcO0MZ6BL6yppS2SFfMBo"
 DEV = 1664850827
 OWNER = "6269431686"
 ffmpegcode = ["-preset faster -c:v libx265 -s 400x310 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2  -ab 32k  -vbr 2 -level 3.1 -threads 1"]
-THUMB = "https://telegra.ph/file/3ef8ee967488070b886f2.jpg"
+THUMBNAIL = "https://telegra.ph/file/3ef8ee967488070b886f2.jpg"

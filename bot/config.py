e071b59f3e1dcf483b3fe23e20afb8ac0429bb01
newacccx <119074899+newacccx@users.noreply.github.com>
2023-08-16 12:22:11 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 
-APP_ID = "11645883"
-API_HASH = "7df53435767884e542f9cd498c0711a3"
-BOT_TOKEN = "5704942988:AAFuv7ZkxzGJVyxcO0MZ6BL6yppS2SFfMBo"
+APP_ID = "6381607"
+API_HASH = "9799ad1623afe9bad664501f984b71fe"
+BOT_TOKEN = "5690536580:AAG3v5sTFBE_dDNnKASsaTZ1c0ZoJNtFGik"
 DEV = 1664850827
-OWNER = "6269431686"
+OWNER = "1258695344"
 ffmpegcode = ["-preset faster -c:v libx265 -s 400x310 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2  -ab 32k  -vbr 2 -level 3.1 -threads 1"]
 THUMBNAIL = "https://telegra.ph/file/3ef8ee967488070b886f2.jpg"
